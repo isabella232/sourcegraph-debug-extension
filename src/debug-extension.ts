630a71724c3fcb10085b2460c48c0f65f95f5856
Quinn Slack <qslack@qslack.com>
2018-11-28 03:45:10 -0800

    track unsubscribables to teardown cleanly

--- src/debug-extension.ts
+++ src/debug-extension.ts
@@ -21,7 +21,9 @@ async function fetchSourcegraphVersion(): Promise<string> {
         })
 }
 
-export function activate(): void {
+export function activate(
+    ctx: sourcegraph.ExtensionContext = { subscriptions: { add: (_unsubscribable: any) => void 0 } }
+): void {
     async function setDecorations(pos: sourcegraph.Position, version: string): Promise<void> {
         const editor = sourcegraph.app.activeWindow ? sourcegraph.app.activeWindow.visibleViewComponents[0] : undefined
 
@@ -39,14 +41,16 @@ export function activate(): void {
         }
     }
 
-    sourcegraph.languages.registerHoverProvider(['*'], {
-        provideHover: (doc, pos) =>
-            fetchSourcegraphVersion()
-                .then(version => {
-                    const value = `Sourcegraph version: ${version}`
+    ctx.subscriptions.add(
+        sourcegraph.languages.registerHoverProvider(['*'], {
+            provideHover: (doc, pos) =>
+                fetchSourcegraphVersion()
+                    .then(version => {
+                        const value = `Sourcegraph version: ${version}`
 
-                    return setDecorations(pos, value).then(() => Promise.resolve(version))
-                })
-                .then(version => ({ contents: { value: version } })),
-    })
+                        return setDecorations(pos, value).then(() => Promise.resolve(version))
+                    })
+                    .then(version => ({ contents: { value: version } })),
+        })
+    )
 }
